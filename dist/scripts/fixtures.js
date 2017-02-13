19be4c1b5ae6e806b9e0c092b971b4c3b5feffa9
Michelangelo Pereira <michelangelo.ph@gmail.com>
2017-02-12 16:17:27 -0800

    Angular project controllers added

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+
+var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+var albumRage = {
+     title: 'Rage Against The Machine',
+     artist: 'Rage Against The Machine',
+     label: 'Epic',
+     year: '1992',
+     albumArtUrl: 'assets/images/album_covers/22.jpg',
+     songs: [
+         { title: 'Bombtrack', duration: '4:03' },
+         { title: 'Killing In The Name', duration: '5:13' },
+         { title: 'Take The Power Back', duration: '5:35'},
+         { title: 'Settle For Nothing', duration: '4:47' },
+         { title: 'Bullet In The Head', duration: '5:07'}
+     ]
+ };
